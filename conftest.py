b6306939cfacbb58662455c327ddcc646029e0d3
Vyacheslav Ershov <v.ershov@ispsystem.com>
2023-02-14 13:18:10 +0800

    change language

--- conftest.py
+++ conftest.py
@@ -1,26 +1,12 @@
 import pytest
-from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver import Firefox
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
-BASE_PATH = 'http://selenium1py.pythonanywhere.com/'
+browser_list = ('chrome', 'firefox')
 language_list = ('ar', 'ca', 'cs', 'da', 'de', 'en-gb', 'el', 'es', 'fi', 'fr', 'it',
                  'ko', 'nl', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sk', 'uk', 'zh-hans')
-
-
-def get_chrome_driver():
-    options = ChromeOptions()
-    options.add_argument('--window-size=1920,1080')
-    driver = webdriver.Chrome(options=options)
-    return driver
-
-
-def get_firefox_driver():
-    options = FirefoxOptions()
-    options.add_argument('--width=1920')
-    options.add_argument('--height=1080')
-    driver = webdriver.Firefox(options=options)
-    return driver
 
 
 def pytest_addoption(parser):
@@ -31,27 +17,40 @@ def pytest_addoption(parser):
 
 
 def check_language(request):
-    lang = request.config.getoption("language")
-    if lang in language_list:
-        return lang
+    user_language = request.config.getoption("language")
+    if user_language in language_list:
+        return user_language
     else:
         raise pytest.UsageError("--language invalid value")
 
 
 def check_browse(request):
     browser_name = request.config.getoption("browser_name")
-    if browser_name == 'chrome':
-        driver = get_chrome_driver()
-    elif browser_name == 'firefox':
-        driver = get_firefox_driver()
+    if browser_name in browser_list:
+        return browser_name
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
-    return driver
+
+
+def get_driver(browser, language):
+    if browser == 'chrome':
+        options = ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        options.add_argument('--window-size=1920,1080')
+        driver = Chrome(options=options)
+        return driver
+
+    if browser == 'firefox':
+        options = FirefoxOptions()
+        options.set_preference('intl.accept_languages', language)
+        driver = Firefox(options=options)
+        return driver
 
 
 @pytest.fixture(scope='function')
 def browser(request):
-    driver = check_browse(request)
+    browser = check_browse(request)
     language = check_language(request)
-    yield driver, language
+    driver = get_driver(browser, language)
+    yield driver
     driver.quit()
--- test_items.py
+++ test_items.py
@@ -1,16 +1,13 @@
 from time import sleep
 from selenium.webdriver.common.by import By
-from conftest import BASE_PATH
 
-PAGE_PATH = '/catalogue/coders-at-work_207'
+url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
 class TestPage:
 
     def test_add_to_basket(self, browser):
-        driver, language = browser
-        url = BASE_PATH + language + PAGE_PATH
-        driver.get(url)
-        # sleep(30)
-        btn = driver.find_elements(By.CSS_SELECTOR, '#add_to_basket_form>button')
+        browser.get(url)
+        #sleep(30)
+        btn = browser.find_elements(By.CSS_SELECTOR, '#add_to_basket_form>button')
         assert btn, 'Not found button - "Add to basket"'
