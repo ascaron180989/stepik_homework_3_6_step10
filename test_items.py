7952a3b6014c89c69257b57ab4b487abb34b482b
Vyacheslav Ershov <v.ershov@ispsystem.com>
2023-02-14 13:21:06 +0800

    change language

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 class TestPage:
 
-    def test_add_to_basket(self, browser):
+    def test_is_add_to_basket(self, browser):
         browser.get(url)
         #sleep(30)
         btn = browser.find_elements(By.CSS_SELECTOR, '#add_to_basket_form>button')
